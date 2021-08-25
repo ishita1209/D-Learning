333cee343962432b83fc314ab0a378bd50c6535a
ishita1209 <66006410+ishita1209@users.noreply.github.com>
2021-08-26 02:05:50 +0530

    Add files via upload

---
+++ Teacher/tlogin.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+$_SESSION['login']="yes";
+$name= $_GET['name'];
+$email=$_GET['email'];
+$_SESSION['email']=$email;
+$_SESSION['name']=$name;
+$con= new mysqli('localhost','root','','apricity_project')or die("Could not connect to mysql".mysqli_error($con));
+$sql= "SELECT * FROM admin WHERE email='$email'";
+$result= mysqli_query($con, $sql);
+$count= mysqli_num_rows($result);
+if($count==0)
+{
+$sql= "INSERT INTO admin(name, email) VALUES ('$name','$email')";
+$result=mysqli_query($con, $sql);
+}
+echo ' <script>window.location.href = "http://localhost:8080/Teacher/homepage.php"; </script>';
+?>
